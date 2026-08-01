ba133631044ea3632cb39e904aac8ba58f0d5119
Aayush Mistri <aayushhmistri@gmail.com>
2026-08-01 22:22:42 +0530

    working fine and ignore the import warning/error(the create_react_agent is still under langgraph.prebuilt not langchain.agents)

--- math.py
+++ math.py
@@ -11,6 +11,16 @@ def add(a:int , b:int)->int:
     """
     return a + b
 
+
+@mcp.tool()
+def multiply(a:int , b:int)->int:
+    """_summary_
+
+        multiply two numbers
+
+    """
+    return a * b
+
 ## The transport="stdio" argument tells the server to :
 ##    Use standard input/output(stdin and stdout) to receive and respond to tool funcion calls
 if __name__ == "__main__":
