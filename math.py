88c2dc85627fe687796e3537b02ad60b0dca3461
Aayush Mistri <aayushhmistri@gmail.com>
2026-08-01 21:09:14 +0530

    fastmcp version mismatched

---
+++ math.py
@@ -0,0 +1,17 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP("Math")
+
+@mcp.tool()
+def add(a:int , b:int)->int:
+    """_summary_
+
+        Add two numbers
+
+    """
+    return a + b
+
+## The transport="stdio" argument tells the server to :
+##    Use standard input/output(stdin and stdout) to receive and respond to tool funcion calls
+if __name__ == "__main__":
+    mcp.run(transport="stdio") 
---
+++ weather.py
@@ -0,0 +1,17 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Weather")
+
+@mcp.tool()
+async def add(location:str)->str:
+    """_summary_
+
+        Get the weather locaion
+
+    """
+    return "It is sunny"
+
+## The transport="stdio" argument tells the server to :
+##    Use standard input/output(stdin and stdout) to receive and respond to tool funcion calls
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
